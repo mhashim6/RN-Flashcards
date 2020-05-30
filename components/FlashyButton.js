f03e8d4a163b84f2304cc498ea2b4f7bfe2a088b
mhashim6 <m6hashim@gmail.com>
2020-05-31 00:30:00 +0200

    common components and styles

---
+++ components/FlashyButton.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { TouchableOpacity, Text, StyleSheet } from 'react-native'
+import FlashyStyles from './shared/flashyStyles'
+
+const FlashyButton = ({ children, onPress, ...props }) => (
+    <TouchableOpacity style={[FlashyStyles.roundedBorder, styles.flashyButton, { backgroundColor: props.disabled ? 'darkgrey' : '#000' }]} {...props} onPress={onPress} >
+        <Text style={styles.flashyText} >{children}</Text>
+    </TouchableOpacity>
+)
+
+const styles = StyleSheet.create({
+    flashyButton: {
+        justifyContent: 'center',
+        alignContent: 'center',
+        backgroundColor: '#000',
+        color: '#fff',
+        height: 46,
+        marginTop: 8,
+
+    },
+    flashyText: {
+        ...FlashyStyles.textInput,
+        textAlign: 'center',
+        color: '#fff',
+    }
+})
+
+export default FlashyButton
---
+++ components/shared/flashyStyles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+const FlashyStyles = StyleSheet.create({
+    roundedBorder: {
+        borderColor: 'grey',
+        borderWidth: 1,
+        borderRadius: 4,
+    },
+    textInput: {
+        fontSize: 20,
+        padding: 8,
+    }
+})
+
+export default FlashyStyles
