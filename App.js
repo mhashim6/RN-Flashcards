a441dd968eb2095f2219be17d84692e65fc78991
mhashim6 <m6hashim@gmail.com>
2020-05-31 00:32:37 +0200

    hollistic app

--- App.js
+++ App.js
@@ -1,19 +1,49 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler'
+import React, { useEffect } from 'react'
+import { StyleSheet, SafeAreaView, AsyncStorage } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+import decksReducer from './deck-state/reducer'
+import middleware from './deck-state/middleware'
+
+import DeckDetail from './components/DeckDetail'
+import Home from './components/Home'
+import { loadStoredDecks } from './deck-state/actions'
+import NewQuestion from './components/NewQuestion'
+import Quiz from './components/Quiz'
+
+
+const store = createStore(decksReducer, middleware)
+const Stack = createStackNavigator()
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadStoredDecks())
+  })
+  return (<Provider store={store}>
+    <NavigationContainer>
+      <SafeAreaView style={styles.container}>
+        <Stack.Navigator initialRouteName='Home'>
+          <Stack.Screen name="Home" options={{ title: 'Flashcards' }} component={Home} />
+          <Stack.Screen name="DeckDetail" options={({ route }) => ({ title: route.params.deck.title })} component={DeckDetail} />
+          <Stack.Screen name="NewQuestion" options={({ route }) => ({ title: route.params.deck.title })} component={NewQuestion} />
+          <Stack.Screen name="Quiz" options={({ route }) => ({ title: route.params.deck.title })} component={Quiz} />
+        </Stack.Navigator>
+      </SafeAreaView>
+    </NavigationContainer>
+  </Provider>)
+};
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+export default App
