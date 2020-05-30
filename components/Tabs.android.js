092733e83b7279fc4bd8696c9aff9bfdfb05f265
mhashim6 <m6hashim@gmail.com>
2020-05-31 00:30:24 +0200

    x-platform Bottom tabs

---
+++ components/Tabs.android.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+
+export default createMaterialBottomTabNavigator()
---
+++ components/Tabs.ios.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+export default createBottomTabNavigator()
